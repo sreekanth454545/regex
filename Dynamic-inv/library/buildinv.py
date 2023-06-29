14c4ffa11c0498f1a4c047421d151bb6a9e49317
Yathish B <35368208+yathishb@users.noreply.github.com>
2023-06-29 20:39:36 +0530

    Code changes related to Dynamic ivn

---
+++ Dynamic-inv/library/buildinv.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import *
+
+
+def chunk_list(my_list, chunk_size):
+    f_list=[]
+    while my_list:
+        chunk, my_list = my_list[:chunk_size], my_list[chunk_size:]
+        f_list.append(chunk)
+    return f_list
+  
+
+
+def main():
+    
+    content=""
+    fields = {
+        "sql_results": {"default": True,"type": "list"},
+        "chunk_len": {"default": True,"type": "int"},
+        "device_grp": {"default": True,"type": "str"}
+    }
+
+    module = AnsibleModule(argument_spec=fields)
+    try:
+        f_list=chunk_list(module.params['sql_results'],module.params['chunk_len'])
+        if len(f_list) >= 0 :
+            for i in range(0,len(f_list)):
+                content=content+'''['''+str(module.params['device_grp'])+str(i+1)+''']\n'''
+                for item in f_list[i]:
+                    content=content+item+"\n"
+            module.exit_json(changed=True,result=content)        
+        else:
+            module.fail_json(msg="List with 0 index",changed=False )
+    except Exception as e:
+        print("Error:",str(e))
+        module.fail_json(msg=str(e),changed=False )
+
+if __name__ == '__main__':
+    main()
