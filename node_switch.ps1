3d33bad0a516d7bc05e3d2b9685da12e94a4d653
Yathish B <35368208+yathishb@users.noreply.github.com>
2023-08-18 21:46:20 +0100

    Update and rename nvm_switch.ps1 to node_switch.ps1
    Code changes related to node version switch

---
+++ node_switch.ps1
@@ -0,0 +1,21 @@
+param (
+    [Parameter(Mandatory=$true)][string]$version=$(Read-Host "Enter the NVM Version")
+)
+$flag=0
+$node_len=0
+$nodejsdir="C:\Users\yathi\AppData\Roaming\nvm"
+Write-Host "Selected Version=$version" -ForegroundColor Yellow
+$nodejspath=$nodejsdir+"\v"+$version
+Write-Host "Path=$nodejspath" -ForegroundColor Yellow
+if (Test-Path -Path $nodejspath) {
+   $node_len=[Environment]::GetEnvironmentVariable("NODE_PATH", "User").Length
+   if ($node_len -eq 0) {
+        Write-Host "Updating path Environment Varaible" -ForegroundColor Blue
+        $Path = [Environment]::GetEnvironmentVariable("PATH", "User") + [IO.Path]::PathSeparator + "%NODE_PATH%" 
+        [Environment]::SetEnvironmentVariable( "Path","$PATH", "User")
+   }
+   setx NODE_PATH "$nodejspath"
+   exit 0
+} else {
+    "Node JS Version is not installed"
+}
