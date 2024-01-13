cca348b8998753e63d29ea4051a161ce913e2a05
Yathish B <35368208+yathishb@users.noreply.github.com>
2024-01-13 09:55:14 +0000

    Update get_host_inv_grp.py

--- dynamicINV/get_host_inv_grp.py
+++ dynamicINV/get_host_inv_grp.py
@@ -1,42 +1,48 @@
-from ansible.parsing.dataloader import DataLoader
-from ansible.inventory.manager import InventoryManager
 from ansible.module_utils.basic import AnsibleModule
+import os
+def get_inv_group(inv_filename,device_grp):
+    device_list=[]
+    try:
+        if os.path.isfile(inv_filename):
+            file=open(inv_filename,"r")
+            content=file.readlines()
+            for i in range(0, len(content)):
+                if str(content[i].strip()) == device_grp:
+                    for j in range(i + 1, len(content)):
+                        if j >= len(content):
+                            if device_list is None:
+                                return 1, "No Matching Device Group Found"
+                            else:
+                                return 0,device_list
+                        if str(content[j].strip()) != "":
+                            if str(content[j].strip()[0]) == "[" and str(content[j].strip()) != device_grp:
+                                return 0,device_list
+                            else:
+                                device_list.append(content[j].strip())
+            if len(device_list) >= 0:
+                return 1, "No Matching Device Group Found"
+            else:
+                return 0,device_list
+        else:
+            raise FileNotFoundError("{} not found".format(inv_filename))
+    except Exception as e:
+        return 1,str(e)
 
 if __name__ == '__main__':
     module_args = dict(inventory_filename=dict(type='str', required=True), device_grp=dict(type='str', required=True))
     result = dict(changed=False, msg='')
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
     try:
-        data_loader = DataLoader()
-        inventory = InventoryManager(loader = data_loader,sources=[module.params['inventory_filename']])
-        inventory_list=inventory.get_groups_dict()[module.params['device_grp']]
-        result['msg'] = inventory_list
-        result['changed'] = True
-        module.exit_json(**result)
+        status_code,inv=get_inv_group(module.params['inventory_filename'],module.params['device_grp'])
+        if status_code == 0:
+            result['msg'] = set(inv)
+            result['changed'] = True
+            module.exit_json(**result)
+        elif status_code >= 1:
+            result['msg'] = "Error While reading the inventory for grp {},Error:{}".format(module.params['device_grp'],inv)
+            result['changed'] = True
+            module.exit_json(**result)
     except KeyError as e:
         result['msg'] = str(e)
         result['changed'] = False
         module.fail_json(**result)
-
-- name: Download the code from the inventory
-      uri:
-        url: "{{gitlab_url}}api/v4/projects/{{gitlab_projectid}}/packages/generic/AristaInventory/LATEST/inventory_new.ini"
-        method: GET
-        headers:
-          PRIVATE-TOKEN: 'glpat-hSdAURpV5FWwRq4dpjx9'
-        status_code: 200
-        return_content: true
-      register: data
-    - name: Write to the File
-      lineinfile:
-        path: "inventory_new.ini"
-        line: "{{ data.content }}"
-        state: present
-        create: true
-    - name: Get Inventory by Group
-      get_hosts_inv_grp:
-        inventory_filename: "inventory_new.ini"
-        device_grp: "{{device_grp}}"
-      register: result
-    - debug:
-        var: result
